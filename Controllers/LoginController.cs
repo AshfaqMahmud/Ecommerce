7095b4f72ec44a98a268d3c76d69c5d4a5583331
Ashfaq Mahmud <ash.mse21@gmail.com>
2024-06-12 04:08:00 +0600

    first-commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Perform your authentication logic here.
+                // For demonstration, we are assuming successful login if username is "admin" and password is "password".
+
+                if (model.Username == "admin" && model.Password == "password")
+                {
+                    // Redirect to home page after successful login.
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form.
+            return View(model);
+        }
+    }
+}
